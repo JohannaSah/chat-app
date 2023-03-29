4dbe718ae3071d805fcb50022a18a2f944caad95
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-29 16:09:15 +0200

    update: add new feature, more comments, new styles

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 // Import required modules
 import { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView, TouchableOpacity, Text, Video } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
@@ -19,21 +19,34 @@ const Chat = ({ route, navigation }) => {
     // Renders a chat bubble with a custom background color based on whether the message is sent by the user or received by the user
     const renderBubble = (props) => {
         return (
+            // The Bubble component that contain the sent messages
             <Bubble 
+                 // Spread all the props passed to the renderBubble function
                 {...props}
+                // Set the styles for the chat bubble wrapper based on whether it's on the right or left
                 wrapperStyle={{
                     right: {
-                      backgroundColor: "#C9EEFF",
+                      backgroundColor: "#E1EEDD",
+                      
                     },
                     left: {
-                      backgroundColor: "#FFDD83",
+                      backgroundColor: "#ECF2FF",
                     },
+                }}
+                // Set the text style for the text inside the chat bubble
+                textStyle={{
+                    right: {
+                      color: "black"
+                    },
+                    left: {
+                      color: "black"
+                    }
                 }}
             />
         )
     };
 
-     // Set the navigation title to the name using useEffect hook
+    // Set the navigation title to the name using useEffect hook
      useEffect( () => {
         navigation.setOptions({ title : name }, );
     }, []);
@@ -67,19 +80,23 @@ const Chat = ({ route, navigation }) => {
                 user: {
                     _id: 2,
                     name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any'
+                    avatar: 'https://placehold.co/600x400/000000/FFFFFF/png'
                 },
                 // Add an image prop:
-                image: 'https://placeimg.com/140/140/any',
-                // Add a video prop:
-                video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+                image: 'https://placehold.co/600x400/000000/FFFFFF/png',
                 // Mark the message as sent, using one tick
                 sent: true,
                 // Mark the message as received, using two tick
                 received: true,
                 // Mark the message as pending with a clock loader
                 pending: true,
-            }
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
+              },
         ])
     }, []);
 
@@ -100,6 +117,22 @@ const Chat = ({ route, navigation }) => {
                 _id: 1
             }}
         />
+
+        {/* A component thatprevents the keyboard from hiding the message input field */}
+        {
+            Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+        }
+
+        {/* A touchable component that navigates to th Start screen when pressed */}
+        <TouchableOpacity 
+            // An object containing style rules for the TouchableOpacity component
+            style={styles.button}
+            title='Go to Start'
+            // A function that navigates to the specified screen while giving it the typed in name and the chosen background color
+            onPress={ () => navigation.navigate('Start') }
+        >
+            <Text> Leave Chat </Text>
+        </TouchableOpacity>
       </View>
     );
    }
@@ -107,10 +140,17 @@ const Chat = ({ route, navigation }) => {
    const styles = StyleSheet.create({
     /* Entire screen */
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      flex: 1
+    },
+    /* Styles Leave Chat button  */
+    button: {
+        alignItems: 'center',
+        backgroundColor: 'lightgrey',
+        padding: 10,
+        borderColor: 'black',
+        borderWidth: 1,
     }
    });
+
 
    export default Chat;
